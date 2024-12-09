34a475f731f86ef772a7f2840e690c7aa7936647
Tsukumo1 <98806776+Tsukumo1@users.noreply.github.com>
2024-12-10 01:55:58 +0800

    Add files via upload

---
+++ eval/data_postprocess_cypher.py
@@ -0,0 +1,97 @@
+import re
+
+def extract_cypher_queries_asfile(input_file, output_file):
+    content=[]
+    with open(input_file, 'r', errors='ignore') as f:
+        for line in f:
+            content.append(line)
+
+    pattern1 = r'```\s*(MATCH[\s\S]*?)```'
+
+    pattern4 = r'```cypher\s*(MATCH[\s\S]*?)```'
+
+    pattern2 = r':\s*(MATCH[\s\S]*?)(?=\n\S|$)'
+
+    pattern3 = r':\s*(MATCH[\s\S]*?);'
+
+    pattern5 = r':\s*(MATCH[\s\S]*?)This'
+
+    pattern6 = r'\*/\s*(MATCH[\s\S]*?)(?=\n\S|$)'
+
+    pattern7 = r'1\. (.*?)2\.'
+
+    pattern8 = r'1\. (.*)$'
+
+    cypher_queries = []
+
+    for i in content:
+        matches1 = re.findall(pattern1, i, re.IGNORECASE)
+        matches2 = re.findall(pattern2, i, re.IGNORECASE)
+        matches3 = re.findall(pattern3, i, re.IGNORECASE)
+        matches4 = re.findall(pattern4, i, re.IGNORECASE)
+        matches5 = re.findall(pattern5, i, re.IGNORECASE)
+        matches6 = re.findall(pattern6, i, re.IGNORECASE)
+        matches7 = re.findall(pattern7, i, re.IGNORECASE)
+        matches8 = re.findall(pattern8, i, re.IGNORECASE)
+        
+        if matches1:
+            str = matches1[0].rstrip('; ') 
+            str = str.replace(";", "")
+            str = str.replace("`", "")
+            cypher_queries.append(str+'\n')
+        elif matches4:
+            str = matches4[0].rstrip('; ')
+            str = str.replace(";", "")
+            str = str.replace("`", "")
+            cypher_queries.append(str+'\n')
+        elif matches3:
+            str = matches3[0].rstrip('; ')
+            str = str.replace(";", "")
+            str = str.replace("`", "")
+            cypher_queries.append(str+'\n')
+        elif matches5:
+            str = matches5[0].rstrip('; ')
+            str = str.replace(";", "")
+            str = str.replace("`", "")
+            cypher_queries.append(str+'\n')
+        elif matches2:
+            str = matches2[0].rstrip('; ')
+            str = str.replace(";", "")
+            str = str.replace("`", "")
+            cypher_queries.append(str+'\n')
+        elif matches6:
+            str = matches6[0].rstrip('; ')
+            str = str.replace(";", "")
+            str = str.replace("`", "")
+            cypher_queries.append(str+'\n')
+        elif matches7:
+            str = matches7[0].rstrip('; ')
+            str = str.replace(";", "")
+            str = str.replace("`", "")
+            cypher_queries.append('MATCH ('+str+'\n')
+        elif matches8:
+            str = matches8[0].rstrip('; ')
+            str = str.replace(";", "")
+            str = str.replace("`", "")
+            cypher_queries.append('MATCH ('+str+'\n')
+        else:
+            str = i.rstrip('; ')
+            str = str.replace(";", "")
+            str = str.replace("`", "")
+            cypher_queries.append(str)
+
+    cypher_queries = [query.rstrip(';') for query in cypher_queries]
+
+    cypher_queries = [query.rstrip('`') for query in cypher_queries]
+
+    with open(output_file, 'w') as f:
+        for query in cypher_queries:
+            f.write(f"{query}")
+
+    print(f"Extracted {len(cypher_queries)} unique cypher queries to {output_file}")
+
+# Usage
+input_file = 'Replace with your input file name' 
+output_file = 'Replace with your desired output file name'  
+
+extract_cypher_queries_asfile(input_file, output_file)
